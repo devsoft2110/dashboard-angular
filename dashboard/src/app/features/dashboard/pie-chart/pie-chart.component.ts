c555483834e7f0233199ca2e8e141b8038ca17c1
Faizy Devsoft <devsoft2110@gmail.com>
2024-04-22 22:03:51 +0500

    Integrated pie chart component

---
+++ dashboard/src/app/features/dashboard/pie-chart/pie-chart.component.ts
@@ -0,0 +1,88 @@
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css'],
+})
+export class PieChartComponent {
+  chartType: 'pie' = 'pie';
+
+  chartLegend = false;
+  chartPlugins = [];
+
+  chartOptions: ChartConfiguration<'pie'>['options'] = {
+    responsive: true,
+    events: ['mousemove'],
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+        backgroundColor: '#FFFFFF',
+        titleColor: 'black',
+        // displayColors: true,
+
+        callbacks: {
+          label: function (context: any) {
+            return context.dataset.data[context.dataIndex];
+          },
+          labelTextColor: function (context) {
+            return 'black';
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        display: true,
+
+        grid: {
+          display: true,
+        },
+        beginAtZero: true,
+      },
+      y: {
+        display: true,
+        grid: {
+          display: true,
+        },
+        beginAtZero: true,
+      },
+    },
+  };
+
+  datasets: any = [
+    {
+      backgroundColor: '#08B6F0',
+      borderColor: '#08B6F0',
+      data: [],
+      barThickness: 25,
+      hoverBackgroundColor: '#08B6F0',
+    },
+    {
+      backgroundColor: '#42D04B',
+      borderColor: '#42D04B',
+      data: [],
+      barThickness: 25,
+      hoverBackgroundColor: '#42D04B',
+    },
+    {
+      backgroundColor: '#D04242',
+      borderColor: '#D04242',
+      data: [],
+      barThickness: 25,
+      hoverBackgroundColor: '#D04242',
+    },
+  ];
+
+  data: ChartConfiguration<'pie'>['data'] = {
+    labels: ['A', 'B', 'C', 'D', 'E'],
+    datasets: [{ data: [10, 40, 25, 55, 20] }],
+  };
+
+  @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
+}
